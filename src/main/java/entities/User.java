812f9f6347de1d0d235cdd1a17ea517b57be7650
HubertPol <hubert_gluchowski@interia.pl>
2018-06-02 13:41:18 +0200

    created user entitie, added new record in database

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,5 +1,17 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
 public class User {
 
+    private Integer id;
+
+    private String login;
+
+    private String email;
+
+    private String displayedName;
 }
