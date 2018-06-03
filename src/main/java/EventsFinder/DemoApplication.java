616b388ac6f9617a03ae49a0c61f2fa477436095
LuCkyLuk <lukson231@gmail.com>
2018-06-03 14:28:57 +0200

    improving entity

--- src/main/java/EventsFinder/DemoApplication.java
+++ src/main/java/EventsFinder/DemoApplication.java
@@ -1,4 +1,4 @@
-package EventsFinder.demo;
+package EventsFinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/EventsFinder/DemoApplicationTests.java
+++ src/test/java/EventsFinder/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package EventsFinder.demo;
+package EventsFinder;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
