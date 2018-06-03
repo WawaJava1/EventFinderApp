ce1951519fc9536ee6fce90864b0268dc03166d9
LuCkyLuk <lukson231@gmail.com>
2018-06-03 12:45:36 +0200

    @

--- src/main/java/entities/Comment.java
+++ src/main/java/entities/Comment.java
@@ -1,20 +1,28 @@
 package entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "COMMENT")
 public class Comment {
 
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idComment")
     private Integer idComment;
 
+    @Column (name = "contents")
     private String contnents;
 
+    @Column (name = "idUser")
     private Integer idUser;
 
+    @Column (name = "idEvent")
     private Integer idEvent;
 
 }
