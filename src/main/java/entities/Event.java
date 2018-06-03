ce12505dfa5ce5fdf2311a238d94198cae4a3686
HubertPol <hubert_gluchowski@interia.pl>
2018-06-03 13:03:58 +0200

    event

--- src/main/java/entities/Event.java
+++ src/main/java/entities/Event.java
@@ -1,19 +1,34 @@
 package entities;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
+@Entity
+@Table(name = "EVENT")
 public class Event {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "idEvent")
     private Integer idEvent;
 
-    private Date date;
+    @Column(name = "startDate")
+    private LocalDateTime startDate;
 
+    @Column(name = "endDate")
+    private LocalDateTime endDate;
+
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "address")
     private String address;
 
+    @Column(name = "access")
     private String access;
 
+    @Column(name = "organizer")
     private String organizer;
 
 
